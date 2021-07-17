97f23510a8bd33fd685614c3b45812e1524674b8
Adam Stegman <me@adamstegman.com>
2021-07-17 13:37:06 -0500

    condense classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,61 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'chore.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ChoresApp());
 
-class MyApp extends StatelessWidget {
+const APP_NAME = 'Chores';
+
+class ChoresApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chores',
+      title: APP_NAME,
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Chores'),
+      home: ChoresHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class ChoresHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(APP_NAME),
       ),
       body: Center(
-        child: Chores(),
+        child: ChoreListView(),
       ),
 
-      // TODO: new Chore component
+      // TODO: NewChore component
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
@@ -66,12 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Chores extends StatefulWidget {
+class ChoreListView extends StatefulWidget {
   @override
-  _ChoresState createState() => _ChoresState();
+  _ChoreListViewState createState() => _ChoreListViewState();
 }
 
-class _ChoresState extends State<Chores> {
+class _ChoreListViewState extends State<ChoreListView> {
   final _chores = <Chore>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
